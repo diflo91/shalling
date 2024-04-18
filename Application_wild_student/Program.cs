bfe00cf2a556bb537969ebbb24423be92ba8fda1
diflo91 <rolsroyce796@gmail.com>
2024-04-18 22:34:36 +0200

    Mon premier commit, construction de la partie Option entre eleve et cours, le code fonctionne correctement (pas de crash detecté) l'app respecte le type d'entrer ainsi que ls option disponible

---
+++ Application_wild_student/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Drawing;
+
+namespace Application_wild_student
+{
+    public class Program
+    {
+
+        public static string wildStudent = @"
+    ███████╗██╗  ██╗ █████╗ ██╗     ██╗     ██╗███╗   ██╗ ██████╗ 
+    ██╔════╝██║  ██║██╔══██╗██║     ██║     ██║████╗  ██║██╔════╝ 
+    ███████╗███████║███████║██║     ██║     ██║██╔██╗ ██║██║  ███╗
+    ╚════██║██╔══██║██╔══██║██║     ██║     ██║██║╚██╗██║██║   ██║
+    ███████║██║  ██║██║  ██║███████╗███████╗██║██║ ╚████║╚██████╔╝
+    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+                                                                                          Developped by Fleury
+    ----------------------------------------------------------------------------------------------------------";
+
+        public static bool Correct = false;
+        public static int ChoixOptionInt;
+        public static string ChoixOption;
+
+
+        static void Main(string[] args)
+        {
+           
+
+            while (true)
+            {
+                // Console.Clear();
+
+                
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(wildStudent);
+                Console.ResetColor();
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Write("    ");
+                
+                Console.WriteLine("1. Elève ");
+                Console.Write("    ");
+                Console.WriteLine("2. Cours");
+                Console.WriteLine("");
+                Console.Write("    ");
+                Console.Write("Veuillez choisir l'option : ");
+
+             
+
+                ChoixOption = Console.ReadLine();
+                Correct = int.TryParse(ChoixOption, out ChoixOptionInt);
+
+               
+
+               while (!Correct)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(wildStudent);
+                    Console.ResetColor();
+                    Console.WriteLine(" ");
+                    Console.Write("    ");
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.Write("Saisie invalide. Veuillez saisir un nombre : ");
+                    Console.ResetColor();
+                    ChoixOption = Console.ReadLine();
+                    Correct = int.TryParse(ChoixOption, out ChoixOptionInt);
+                } 
+
+
+               if (ChoixOption == "1" && Correct)
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(wildStudent);
+                        Console.ResetColor();
+                        Console.WriteLine(" ");
+                        Console.Write("    ");
+                        Console.WriteLine("1");
+                        Console.Read();
+                    }
+                
+                } else if (ChoixOption == "2" && Correct)
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(wildStudent);
+                        Console.ResetColor();
+                        Console.WriteLine(" ");
+                        Console.Write("    ");
+                        Console.WriteLine("1");
+                        Console.Read();
+                    }
+
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.Write("    ");
+                    Console.Write("Saisissez les options disponibles, Appuyez enter pour continuer : ... ");
+                    Console.ResetColor();
+                    ChoixOption = Console.ReadLine();
+                    Console.Clear() ;   
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
