9b6accbfa251fa83b83d7c2d68cf46b3f0769545
diflo91 <rolsroyce796@gmail.com>
2024-04-22 17:47:03 +0200

    Ajout de la classe etudiant avec enregistrement dans le fichiet JSON

---
+++ Application_wild_student/Eleves.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Application_wild_student
+{
+    public class Eleves
+    {
+        private string MonCheminJson = "MonFichierJson.json";
+        private int _IdentifiantUniqueEleve;
+        private string _NomEleve;
+        private string _PrenomEleve;
+        private string _DateDeNaissanceEleve;
+       // private Dictionary<string, double> _ListDeNote = new Dictionary<string, double>();
+       // private double _MoyenneNoteEleve;
+
+        public int Identifiant
+        {
+            get { return _IdentifiantUniqueEleve; }
+            set { _IdentifiantUniqueEleve = value; }
+        }
+
+        public string NomEleve
+        {
+            get { return _NomEleve; }
+            set { _NomEleve = value; }
+        }
+
+        public string PrenomEleve
+        {
+            get { return _PrenomEleve; }
+            set { _PrenomEleve = value; }
+        }
+
+        public string DateDeNaissanceEleve
+        {
+            get { return _DateDeNaissanceEleve; }
+            set { _DateDeNaissanceEleve = value; }
+        }
+/*
+        public Dictionary<string, double> ListeNote
+        {
+            get { return _ListDeNote; }
+            set { _ListDeNote = value; }
+        }
+
+        public double MoyenneNoteEleve
+        {
+            get { return _MoyenneNoteEleve; }
+            set { _MoyenneNoteEleve = value; }
+        }
+*/
+        public Eleves(string NewNomEleve, string NewPrenomEleve, string NewDateDeNaissanceEleve/*, Dictionary<string, double> NewListDeNote*/)
+        {
+            _NomEleve = NewNomEleve;
+            _PrenomEleve = NewPrenomEleve;
+            _DateDeNaissanceEleve = NewDateDeNaissanceEleve;
+           // _ListDeNote = NewListDeNote;
+        }
+
+     
+        public void GenIdentifiantEleve()
+        {
+            _IdentifiantUniqueEleve++;
+        }
+
+      
+        public void AjouterEleve(string NewNomEleve, string NewPrenomEleve, string NewDateDeNaissanceEleve /*, Dictionary<string, double> NewListe, double NewMoyenneNoteEleve*/)
+        {
+            _NomEleve = NewNomEleve;
+            _PrenomEleve = NewPrenomEleve;
+            _DateDeNaissanceEleve = NewDateDeNaissanceEleve;
+           
+        }
+
+
+        public void _EleveEnregistrerDonneesJson()
+        {
+            List<Eleves> listeEleves = new List<Eleves>();
+
+            if (File.Exists(MonCheminJson))
+            {
+
+
+                string jsonData = File.ReadAllText(MonCheminJson);
+                listeEleves = JsonConvert.DeserializeObject<List<Eleves>>(jsonData) ?? new List<Eleves>();
+                foreach (var eleve in listeEleves)
+                {
+                    _IdentifiantUniqueEleve = eleve.Identifiant + 1;
+                }
+            }
+
+            
+            listeEleves.Add(this);
+
+            string jsonMiseAJour = JsonConvert.SerializeObject(listeEleves, Formatting.Indented);
+
+            File.WriteAllText(MonCheminJson, jsonMiseAJour);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("    ");
+            Console.Write("    ");
+            Console.WriteLine($"Les données de l'élève ont été ajoutées et enregistrées avec succès.");
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+
+    }
+}
